ced0153919f5b0df7096403d6594740e3908e27d
De Oliveira Tristan <tristan@koji.fr>
2020-04-22 13:22:17 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
 # Exit if any subcommand fails
-set -e 
-
-if [ -z "$1" ]; then
-    glob="."
-else 
-    glob="$@"
-fi
+set -e
 
 echo "## Running ESLint"
 sh -c "yarn --non-interactive lint"
